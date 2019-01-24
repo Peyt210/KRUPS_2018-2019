8a235d7321a72c9e5a72f395a9c3143f41dc14d9
jmmi244 <jmmi244@g.uky.edu>
2019-01-24 11:50:05 -0500

    Added comments
    Comment as you go Peyton............

--- KRUPS_MAIN.ino
+++ KRUPS_MAIN.ino
@@ -1,7 +1,25 @@
-#define Power_on (0)
+/*
+KRUPS 2018-2019 Software
+Sponsored by Dr. Alexandre Martin, University of Kentucky
+Authors: Jordan Johnston, 
+*/
 
-void setup()
-{
-  pinMode(Power_on, OUTPUT);
-  digitalWrite(Power_on, HIGH);
+// All the libraries used for entire project
+#include <Arduino.h>
+#include <SPI.h>
+
+
+// Constants
+#define power_on	0	// GPIO pin used for switching on the battery
+
+void setup(){
+  pinMode(power_on, OUTPUT);
+  digitalWrite(power_on, HIGH);		// Set to output and write high to turn on battery
+  
+  
+  
+}
+
+void loop(){
+	
 }
